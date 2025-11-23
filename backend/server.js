27e15303e8d45c90681553055394c1c654f59fe7
Fernanda Dourado <fernandadourado.m.s@gmail.com>
2025-11-23 03:34:09 -0300

    version 1

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Envio de e-mail (Nodemailer)
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
